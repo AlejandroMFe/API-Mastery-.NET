64e190d6a2cd8bb1d3a2e89c386f7da693d699dd
Alejandro <alejandro.mfe@gmail.com>
2023-09-15 23:00:42 -0300

    Add user registration request class

---
+++ API.FurnitureStore.Shared/DTOs/UserRegistrationRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace API.FurnitureStore.Shared.DTOs;
+public class UserRegistrationRequestDto
+{
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string EmailAddress { get; set; }
+    [Required]
+    public string Password { get; set; }
+}
