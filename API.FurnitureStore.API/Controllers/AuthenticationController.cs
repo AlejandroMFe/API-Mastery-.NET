295f28aaa03d821ef84addd4c8b9b14e361a1fee
Alejandro <alejandro.mfe@gmail.com>
2023-09-15 23:01:13 -0300

    Add Authentication Cotnroller and Register endpoint

---
+++ API.FurnitureStore.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,73 @@
+﻿using API.FurnitureStore.Shared.Auth;
+using API.FurnitureStore.Shared.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+namespace API.FurnitureStore.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly JwtConfig _jwtConfig;
+
+    public AuthenticationController(UserManager<IdentityUser> userManager,
+                                    IOptions<JwtConfig> jwtConfig)
+    {
+        _userManager = userManager;
+        _jwtConfig = jwtConfig.Value;
+    }
+
+    public async Task<IActionResult> Register([FromBody] UserRegistrationRequestDto request)
+    {
+        if (ModelState.IsValid is false) return BadRequest();
+
+        // Verify if the email is already taken
+        var emailExistis = await _userManager.FindByEmailAsync(request.EmailAddress);
+
+        // email already taken
+        if (emailExistis is not null) return BadRequest(new AuthResult()
+        {
+            Result = false,
+            Errors = new List<string>() { "Email already in use" }
+        });
+
+        // Create new user
+        var user = new IdentityUser()
+        {
+            Email = request.EmailAddress,
+            UserName = request.EmailAddress
+        };
+
+        var isCreated = await _userManager.CreateAsync(user);
+
+        if (isCreated.Succeeded)
+        {
+            var token = GenerateToken(user);
+            return Ok(new AuthResult()
+            {
+                Result = true,
+                Token = token
+            });
+        }
+        else
+        {
+            var error = new List<string>();
+            foreach (var err in isCreated.Errors)
+                error.Add(err.Description);
+
+            return BadRequest(new AuthResult()
+            {
+                Result = false,
+                Errors = error
+            });
+        }
+
+        // Something went wrong
+        return BadRequest(new AuthResult()
+        {
+            Result = false,
+            Errors = new List<string>() { "User couldn´t be created" }
+        });
+    }
+}
--- API.FurnitureStore.API/GlobalUsings.cs
+++ API.FurnitureStore.API/GlobalUsings.cs
@@ -1,9 +1,10 @@
-global using API.FurnitureStore.Data;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.AspNetCore.Mvc;
-global using API.FurnitureStore.Shared;
-global using API.FurnitureStore.API;
-global using API.FurnitureStore.API.Configuration;
-global using Microsoft.IdentityModel.Tokens;
-global using Microsoft.AspNetCore.Authentication.JwtBearer;
 global using System.Text;
+
+global using API.FurnitureStore.API.Configuration;
+global using API.FurnitureStore.Data;
+global using API.FurnitureStore.Shared;
+
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.IdentityModel.Tokens;
