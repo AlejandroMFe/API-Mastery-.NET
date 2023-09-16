c7b8eb0c83ae2b2e515cc9f359c78852d4705b71
Alejandro <alejandro.mfe@gmail.com>
2023-09-15 23:00:22 -0300

    Add Auth Result class

---
+++ API.FurnitureStore.Shared/Auth/AuthResult.cs
@@ -0,0 +1,7 @@
+﻿namespace API.FurnitureStore.Shared.Auth;
+public class AuthResult
+{
+    public string Token { get; set; }
+    public bool Result { get; set; }
+    public IEnumerable<string> Errors { get; set; }
+}
