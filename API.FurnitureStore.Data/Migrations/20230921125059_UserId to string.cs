658d7644c61d1efa2e65aa2d2edceb5fe654f1d2
Alejandro <alejandro.mfe@gmail.com>
2023-09-21 10:23:19 -0300

    UserId to string in table

---
+++ API.FurnitureStore.Data/Migrations/20230921125059_UserId to string.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.FurnitureStore.Data.Migrations
+{
+    public partial class UserIdtostring : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "RefreshTokens",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "RefreshTokens",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
